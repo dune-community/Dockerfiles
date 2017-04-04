2c1712c6262a2ff670da8004ae61929b39c32659
Felix Schindler <felix.schindler@wwu.de>
2017-04-04 14:05:38 +0200

    [license] add *.sh to cfg, add header

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# This file is part of the dune-community/Dockerfiles project:
+#   https://github.com/dune-community/Dockerfiles
+# Copyright 2010-2017 dune-community/Dockerfiles developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+# Authors:
+#   Felix Schindler (2017)
 
 if (( "$#" < 3 )); then
   echo ""
