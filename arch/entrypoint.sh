2f795252f0078da7293cd39c0feb1723c55a0a2f
Felix Schindler <felix.schindler@wwu.de>
2017-04-04 14:07:17 +0200

    [license] fix years

--- arch/entrypoint.sh
+++ arch/entrypoint.sh
@@ -2,7 +2,7 @@
 #
 # This file is part of the dune-community/Dockerfiles project:
 #   https://github.com/dune-community/Dockerfiles
-# Copyright 2010-2017 dune-community/Dockerfiles developers and contributors. All rights reserved.
+# Copyright 2017 dune-community/Dockerfiles developers and contributors. All rights reserved.
 # License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 #      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 # Authors:
