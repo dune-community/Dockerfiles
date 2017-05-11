a498f75ce11cb499301fefff4b2c093f36f5cdda
Felix Schindler <felix.schindler@wwu.de>
2017-05-11 13:46:20 +0200

    [debian] allow sudo for apt

--- debian/minimal/entrypoint.sh
+++ debian/minimal/entrypoint.sh
@@ -22,6 +22,7 @@ fi
 chown -R $USERNAME_:$GID_ /home/$USERNAME_
 
 echo "$USERNAME_ ALL=(ALL) NOPASSWD:/usr/bin/apt-get" >> /etc/sudoers
+echo "$USERNAME_ ALL=(ALL) NOPASSWD:/usr/bin/apt" >> /etc/sudoers
 
 export LANG=en_US.UTF-8
 
