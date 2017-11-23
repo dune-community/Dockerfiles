b898ee7f6557659cc1f132df41d12eb4adc900b3
René Milk <rene.milk@wwu.de>
2017-11-23 10:49:09 +0100

    [ci] prune docker before build

--- rules.mk
+++ rules.mk
@@ -25,6 +25,7 @@ $(REPONAMES): check_client
 		-D AUTHOR="$(AUTHOR)" \
 		-D GITREV=$(GITREV) \
 		-I$(THISDIR)/include -I ./include $@/Dockerfile.in > $@/Dockerfile
+	[[ ${DOCKER_PRUNE} ]] && docker system prune -f
 	cd $@ && $(DOCKER_SUDO) docker build ${DOCKER_QUIET} -t $(REPO):$(GITREV) .
 	$(DOCKER_SUDO) docker tag $(REPO):$(GITREV) $(REPO):latest
 
