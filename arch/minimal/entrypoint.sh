2c426141a2fb8e5550215f3d1cea691334016408
Felix Schindler <felix.schindler@wwu.de>
2020-10-10 21:38:34 +0200

    update entrypoint to work with rootless docker and be more conservative

--- arch/minimal/entrypoint.sh
+++ arch/minimal/entrypoint.sh
@@ -9,26 +9,42 @@
 #   Felix Schindler (2017)
 #   Rene Milk       (2017)
 
-set -e
-
-USERNAME_=${LOCAL_USER:-user}
-UID_=${LOCAL_UID:-1000}
+USERNAME_=${LOCAL_USER:-root}
+UID_=${LOCAL_UID:-0}
 GID_=${LOCAL_GID:-$UID_}
 
-groupadd -o -g $GID_ $USERNAME_
-if [ -e /home/$USERNAME_ ] ; then
-  useradd -d /home/$USERNAME_ -g $GID_ -s /bin/bash -u $UID_ $USERNAME_
-else
-  useradd -md /home/$USERNAME_ -g $GID_ -s /bin/bash -u $UID_ $USERNAME_
+export LANG=en_US.UTF-8
+echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
+
+# we are running as root, if UID_ == 0, there is nothing we can do but continue ...
+if [[ $UID_ == 0 ]] ; then
+  if [ "X$@" == "X" ]; then
+    exec /bin/bash
+  else
+    exec "$@"
+  fi
+  exit
 fi
 
-chown -R $USERNAME_:$GID_ /home/$USERNAME_
+# ... else, check if there exist a group and create it otherwise
+[[ $(getent group $GID_) ]] || groupadd -g $GID_ $USERNAME_ &> /dev/null
 
-echo "$USERNAME_ ALL=(ALL) NOPASSWD:/usr/bin/pacman" >> /etc/sudoers
+# check if a user with id $UID_ already exists
+if [[ $(id -u $UID_ &> /dev/null) ]]; then
+  # unconditionally set the primary group of the user
+  usermod -g $GID_ $UID_
+  # rename the user if required
+  [[ "$USERNAME_" == "$(id -un $UID_)" ]] || usermod -l $USERNAME_ $(id -un $UID_)
+else
+  # otherwise create the user
+  [[ -e /home/$USERNAME_ ]] && export m_= || export m_=m
+  useradd -${m_}d /home/$USERNAME_ -g $GID_ -s /bin/bash -u $UID_ $USERNAME_
+fi
 
-export LANG=en_US.UTF-8
-echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
-echo 'cd $HOME' >> /home/$USERNAME_/.bash_profile
+# give the user some sudo capabilities
+if [[ $UID_ != 0 ]] ; then
+  echo "$USERNAME_ ALL=(ALL) NOPASSWD:/usr/bin/pacman" >> /etc/sudoers
+fi
 
 if [ "X$@" == "X" ]; then
   exec su-exec $USERNAME_ /bin/bash
