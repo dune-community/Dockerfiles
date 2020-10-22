3af956365d596c9d0b0aed1a963ed4ff0c98d63d
René Fritze <rene.fritze@wwu.de>
2020-10-22 10:31:22 +0200

    current wheel status

--- manylinux/2014/build-wheels.sh
+++ manylinux/2014/build-wheels.sh
@@ -1,17 +1,31 @@
 #!/bin/bash -l
-set -e -x
+set -ex
+
+cd ${DUNE_SRC_DIR}
+rm -rf dune-uggrid dune-testtools
+
+OPTS=${DUNE_SRC_DIR}/config.opts/manylinux
+
+source /etc/profile.d/pybin.sh
+cd ${DUNE_SRC_DIR}
+./dune-common/bin/dunecontrol --opts=${OPTS} all
+for md in xt gdt xt-data ; do
+  ./dune-common/bin/dunecontrol --only=dune-${md} --opts=${OPTS} make bindings
+done
 
 # do not build from dirty git unless DIRTY_BUILD evaluates to true
-pushd /io/pymor
+pushd /io/dxt
 if [[ $(git rev-parse --show-toplevel 2>/dev/null) = "$PWD" ]] ; then
     [[ ${DIRTY_BUILD} ]] || git diff --exit-code ':(exclude)setup.cfg'
 fi
 popd
 
 # Compile wheels
-${PYBIN}/pip wheel /io/pymor/ -w ${WHEEL_DIR}/
+for md in xt gdt xt-data ; do
+  ./dune-common/bin/dunecontrol --only=dune-${md} --opts=${OPTS} bexec pip wheel python -w ${WHEEL_DIR}/
+done
 
 # Bundle external shared libraries into the wheels
-for whl in ${WHEEL_DIR}/pymor*.whl; do
+for whl in ${WHEEL_DIR}/dune*.whl; do
     auditwheel repair --plat ${PLATFORM} $whl -w ${WHEEL_DIR}/
 done
--- manylinux/2014/entrypoint.sh
+++ manylinux/2014/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-USER_ID=${LOCAL_USER_ID:-1000}
-GROUP_ID=${LOCAL_GROUP_ID:-1000}
+USER_ID=${LOCAL_UID:-1000}
+USER=${LOCAL_USER:-dxt}
+GROUP_ID=${LOCAL_GID:-1000}
 
 echo "Starting with UID : $USER_ID"
-useradd  --shell /bin/bash -u $USER_ID -o  -m pymor
-export HOME=/home/pymor
+groupadd -g ${GROUP_ID} -o ${USER}
+useradd  --shell /bin/bash -u $USER_ID -g ${GROUP_ID} -o  -m ${USER}
+export HOME=/home/${USER}
 
-exec gosu pymor "$@"
+exec gosu ${USER} "$@"
