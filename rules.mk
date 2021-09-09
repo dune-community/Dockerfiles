ec02aa94092fd804bf6f5fcd9d53029a259ba990
René Fritze <rene.fritze@wwu.de>
2021-09-09 10:25:31 +0200

    prevent dirty git tags

--- rules.mk
+++ rules.mk
@@ -12,15 +12,21 @@ REPONAMES = $(patsubst %/,%,$(dir $(wildcard */Dockerfile.in)))
 DOCKER_NOCACHE=
 BUILD_CMD=$(DOCKER_SUDO) docker build ${DOCKER_NOCACHE} --rm=true ${DOCKER_QUIET}
 REGISTRY=zivgitlab.wwu.io/ag-ohlberger/dune-community/docker
+GITREV?=$(shell git log -1 --pretty=format:"%H")
 
-.PHONY: push all $(REPONAMES) readme
+.PHONY: push all $(REPONAMES) readme IS_DIRTY
+
+IS_DIRTY:
+	@cd $(THISDIR) ; \
+	@git diff-index --quiet HEAD || \
+	(git update-index -q --really-refresh && git diff --no-ext-diff --quiet --exit-code) || \
+	(git diff --no-ext-diff ; exit 1)
 
 check_client:
 	@$(DOCKER_SUDO) docker info > /dev/null  || \
 	  (echo "cannot connect to docker client. export DOCKER_SUDO=sudo ?" ; exit 1)
 
-$(REPONAMES): check_client
-	$(eval GITREV=$(shell git describe --tags --dirty --always --long))
+$(REPONAMES): check_client IS_DIRTY
 	$(eval IMAGE=$(NAME)-$@)
 	$(eval REPO=$(REGISTRY)/$(IMAGE))
 	$(eval DF=Dockerfile.generated.$(DEBIANVERSION))
